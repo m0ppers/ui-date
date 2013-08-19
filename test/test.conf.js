06b528738eac039f0f4cf645868880698cb6c26e
Andreas Streichardt <andreas.streichardt@gmail.com>
2013-08-19 16:13:20 +0200

    update to newer format

--- test/test.conf.js
+++ test/test.conf.js
@@ -1,13 +1,16 @@
-basePath = '..';
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'bower_components/jquery/jquery.js',
-  'bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
-  'bower_components/angular/angular.js',
-  'bower_components/angular-mocks/angular-mocks.js',
-  'src/date.js',
-  'test/*.spec.js'
-];
-singleRun = true;
-browsers = [ 'Chrome' ];
+module.exports = function(config) {
+  config.set({
+    basePath: '..',
+    frameworks: ['jasmine'],
+    files: [
+      'bower_components/jquery/jquery.js',
+      'bower_components/jquery-ui/ui/minified/jquery-ui.min.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'src/date.js',
+      'test/*.spec.js'
+    ],
+    singleRun:true,
+    browsers: [ 'Chrome' ]
+  });
+};
